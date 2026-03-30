d6be471806e651e14b0ee8bcc7ef0f3eb093a333
thanosisinevitable <ephorisrevival@proton.me>
2026-03-30 16:18:53 +0530

    add jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,9 @@
+package midterm;
+
+public class jenkins {
+	public static void main(String[] args) {
+		System.out.println("Nandan P R");
+		System.out.println("23MIC0065");
+		System.out.println("Jenkins Build Successful");
+	}
+}
